82e9e54fc88812df8733c6cc0d7a4a37d79efb3c
Louis <louis@Louiss-Mac-mini.local>
2020-04-29 22:01:05 -0400

    Added form validation

--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function useFormValidation(initialState) {
+function useFormValidation(initialState, validate) {
   const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (isSubmitting) {
+      const noErrors = Object.keys(errors).length === 0;
+      if (noErrors) {
+        console.log('authenticated', values);
+        setIsSubmitting(false);
+      } else {
+        setIsSubmitting(false);
+      }
+    }
+  }, [errors]);
 
   function handleChange(e) {
     e.persist();
@@ -11,13 +25,29 @@ function useFormValidation(initialState) {
     }));
   }
 
+  function handleBlur() {
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+    setIsSubmitting(true);
 
     console.log({ values });
   }
 
-  return { handleSubmit, handleChange, values };
+  return {
+    handleSubmit,
+    handleBlur,
+    errors,
+    isSubmitting,
+    handleChange,
+    values,
+  };
 }
 
 export default useFormValidation;
