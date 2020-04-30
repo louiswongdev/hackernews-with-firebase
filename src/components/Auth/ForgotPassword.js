81c7771fa8043c090f80edb4a17bd2f310506327
Louis <louis@Louiss-Mac-mini.local>
2020-04-30 17:31:52 -0400

    Added logout and forgot password functionality

--- src/components/Auth/ForgotPassword.js
+++ src/components/Auth/ForgotPassword.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useContext } from 'react';
+import { FirebaseContext } from '../../firebase';
 
 function ForgotPassword() {
-  return <div>ForgotPassword</div>;
+  const { firebase } = useContext(FirebaseContext);
+  const [resetPasswordEmail, setResetPasswordEmail] = useState('');
+  const [isPasswordReset, setIsPasswordReset] = useState(false);
+  const [passwordResetError, setPasswordResetError] = useState(null);
+
+  async function handResetPassword() {
+    try {
+      await firebase.resetPassword(resetPasswordEmail);
+      setIsPasswordReset(true);
+      setPasswordResetError(null);
+    } catch (err) {
+      console.error('Error in sending email', err);
+      setPasswordResetError(err.message);
+      setIsPasswordReset(false);
+    }
+  }
+
+  return (
+    <div>
+      <input
+        type="email"
+        name="email"
+        className="input"
+        placeholder="Please enter your email"
+        onChange={e => setResetPasswordEmail(e.target.value)}
+      />
+      <div>
+        <button className="button" onClick={handResetPassword}>
+          Reset Password
+        </button>
+      </div>
+      {isPasswordReset && <p>Check email to reset password</p>}
+      {passwordResetError && <p className="error-text">{passwordResetError}</p>}
+    </div>
+  );
 }
 
 export default ForgotPassword;
